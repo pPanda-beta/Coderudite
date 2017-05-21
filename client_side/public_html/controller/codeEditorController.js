7f66ed58915cec4309b44a91510d468cd87fd507
pPanda-beta <palashkok3@gmail.com>
2017-05-22 04:06:54 +0530

    [Palash] Implements code editor

--- client_side/public_html/controller/codeEditorController.js
+++ client_side/public_html/controller/codeEditorController.js
@@ -1,3 +1,24 @@
 angular.module('mainNgApp').controller('codeEditorController', function ($scope) {
 	console.log('Code editor started');
+
+	$scope.types = ['c', 'java'];
+
+	$scope.sol = {
+		type: 'x'
+	};
+
+	$scope.result = {};
+
+	$scope.modeChanged = function () {
+		var aceThemes = {
+			c: "ace/mode/c_cpp",
+			java: "ace/mode/java"
+		};
+		editor.session.setMode(aceThemes[$scope.sol.type]);
+	};
+
+	$scope.submitSolution = function () {
+		$scope.sol.src = editor.getValue();
+		console.log($scope.sol);
+	};
 });
