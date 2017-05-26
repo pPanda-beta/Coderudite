08267d361d1e79451ccbc24e9735842943ccaf8e
punitrokzz <punitsigma47@gmail.com>
2017-05-26 12:47:03 +0530

    [Palash] Implements persistence of problem in client side

--- client_side/public_html/controller/problemController.js
+++ client_side/public_html/controller/problemController.js
@@ -2,9 +2,12 @@ angular.module('mainNgApp').controller('problemController', function ($scope) {
 
 //	$scope.problem = {};
 
-	app.getProblemById(app.getCurrentProblemId())
-			.onsuccess(function (problem1) {
-				$scope.problem = problem1;
-				$scope.$apply();
-			});
+//	app.getProblemById(app.getCurrentProblemId())
+//			.onsuccess(function (problem1) {
+//				$scope.problem = problem1;
+//				$scope.$apply();
+//			});
+
+	if (app.state.currentProblem !== undefined)
+		$scope.problem = app.state.currentProblem;
 });
