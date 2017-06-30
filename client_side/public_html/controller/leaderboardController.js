53c64c7e0ef934d5adec8992c1152a5cacbb4cca
pPanda-beta <palashkok3@gmail.com>
2017-07-01 00:04:23 +0530

    [Palash] [client] implements working leaderboard

--- client_side/public_html/controller/leaderboardController.js
+++ client_side/public_html/controller/leaderboardController.js
@@ -1,3 +1,11 @@
 angular.module('mainNgApp').controller('leaderboardController', function ($scope) {
-	$scope.x = 'Leaderboard...';
+
+	app.requestJson('/leaderboard', {})
+			.onsuccess(function (ldb) {
+				$("#example").dataTable({
+					data: ldb.body,
+					columns: ldb.header.map((hd) => ({title: hd})),
+					iDisplayLength: -1
+				});
+			});
 });
